26f1a8db18e9e8ee78c669ae7704e5478b756fa1
Sergey Shappo <shappoff@gmail.com>
2023-04-05 23:28:55 +0300

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+const resultBotUrl = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/getUpdates`;
+
+fetch(resultBotUrl)
+    .then((r) => r.json())
+    .then((ctx) => {
+        if (ctx.result) {
+            ctx.result.forEach(({message}) => {
+                if (message) {
+                    const {
+                        from,
+                        message_id,
+                        chat,
+                        date,
+                        text = '',
+                    } = message;
+                    const username = `${from.first_name ? from.first_name : ''} ${from.last_name ? from.last_name : ''} ${from.username ? from.username : ''}`.trim()
+
+                    console.log(new Date( +`${date}000`).toDateString(), chat.username, message_id, `(${username})`, text);
+                }
+            });
+        }
+    });
